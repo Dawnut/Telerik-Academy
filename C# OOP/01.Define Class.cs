d8ae484ca9485e868126fcd08e7a61030dd88d14
Dawnut <Dawnut.x@gmail.com>
2016-06-12 16:22:20 +0300

    test commit

---
+++ C# OOP/01.Define Class.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSM.Components
+{
+    class Program
+    {
+        static void Main()
+        {
+            var battery1 = new Battery("Toshiba","NiCd", 24, 10);
+            var display1 = new Display(5.2, 16000);
+            var phone = new GSM("asdasd", "lenovo", battery: battery1);
+
+            Console.WriteLine((phone.Battery).ToString());
+        }
+    }
+}
---
+++ C# OOP/GSM Components/GSM.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSM.Components
+{
+    public class GSM
+    {
+        private string model;
+        private string manufacturer;
+        private decimal? price;
+        private string owner;
+        private Battery battery;
+        private Display display;
+
+        public string Model
+        {
+            get { return this.model; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Model must be set!");
+                }
+
+                this.model = value;
+            }
+        }
+        public string Manufacturer
+        {
+            get { return this.manufacturer; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Manufacturer must be set!");
+                }
+
+                this.model = value;
+            }
+        }
+        public decimal? Price
+        {
+            get { return this.price; }
+            set { this.price = value; }
+        } 
+
+        public string Owner
+        {
+            get { return this.owner; }
+            set { this.owner = value; }
+        } 
+
+        public Battery Battery
+        {
+            get { return this.battery; }
+            set { this.battery = value; }
+        }
+        public Display Display
+        {
+            get { return this.display; }
+            set { this.display = value; }
+        }
+
+        // Only one constructor for all cases (has optional parameters)
+        public GSM (string model,        
+                    string manufacturer,
+                    decimal? price = null,
+                    string owner = null,
+                    Battery battery = null,
+                    Display display = null)
+        {
+            this.Model = model;
+            this.Manufacturer = manufacturer;
+            this.Price = price;
+            this.Owner = owner;
+            this.Battery = battery;
+            this.Display = display;
+
+        }
+
+        public override string ToString()
+        {
+
+        }
+
+    }
+}
