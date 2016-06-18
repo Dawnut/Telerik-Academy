2234e51634708541e4de4e1959ace86d0d42695d
Dawnut <Dawnut.x@gmail.com>
2016-06-19 00:14:50 +0300

    Revert "Revert "Added second homework""
    This reverts commit df00f7220d0df99e50388030b67f50a4f8d0c397.

---
+++ C# OOP/02. Defining-Classes-Part-2/3DSpace/Distance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenericClass;
+
+namespace _3DSpace
+{
+    public static class Distance
+    {
+        public static double Calculate(Point3D firstPoint, Point3D secondPoint)
+        {
+            return Math.Round(Math.Sqrt(Math.Pow(firstPoint.X - secondPoint.X, 2)
+                             + Math.Pow(firstPoint.Y - secondPoint.Y, 2)
+                             + Math.Pow(firstPoint.X - secondPoint.X, 2)), 1);
+        }
+    }
+}
