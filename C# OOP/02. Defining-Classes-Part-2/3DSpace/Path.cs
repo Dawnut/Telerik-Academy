6091d14eadcd51762bd176cb61d35ae0e3cef849
Dawnut <Dawnut.x@gmail.com>
2016-06-19 00:16:23 +0300

    Revert "Revert "Added second homework""
    This reverts commit 4cfd0a20743246269567d69e7feafa3be8c2df4d.

---
+++ C# OOP/02. Defining-Classes-Part-2/3DSpace/Path.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenericClass;
+
+namespace _3DSpace
+{
+    public class Path
+    {
+        public List<Point3D> PointSequence { get; set; }
+
+        public Path()
+        {
+            this.PointSequence = new List<Point3D>();
+        }
+
+        public void AddPoint(Point3D newPoint)
+        {
+            this.PointSequence.Add(newPoint);
+        }
+        public void RemovePoint(int indexOfPoit)
+        {
+            this.PointSequence.Remove(PointSequence[indexOfPoit-1]);
+        }
+
+        public override string ToString()
+        {
+            var output = new StringBuilder();
+
+            for (int i = 0; i < PointSequence.Count; i++)
+            {
+                output.AppendFormat("{0}. X({1}), Y({2}), Z({3})\n", i + 1, PointSequence[i].X, PointSequence[i].Y, PointSequence[i].Z);
+            }
+
+            return output.ToString().TrimEnd('\n');
+        }
+    }
+}
---
+++ C# OOP/02. Defining-Classes-Part-2/3DSpace/PathStorage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenericClass;
+
+namespace _3DSpace
+{
+    public class PathStorage
+    {
+        public static void SavePath (Path Path)
+        {
+            var writer = new StreamWriter(@"..\..\paths.txt", false);
+
+            using (writer)
+            {
+                foreach (var item in Path.PointSequence)
+                {
+                    writer.WriteLine("{0} {1} {2}", item.X, item.Y, item.Z);
+                    
+                }
+                writer.Close();
+            }
+        }
+
+        public static Path LoadPath()
+        {
+            var reader = new StreamReader(@"..\..\paths.txt");
+            var path = new Path();
+
+            using (reader)
+            {
+                string currentLine = reader.ReadLine();
+                while (currentLine != null)
+                {
+                    double[] coordinates = currentLine.Split(new char[] {' '}).Select(x => double.Parse(x)).ToArray();
+                    path.AddPoint(new Point3D(coordinates[0], coordinates[1], coordinates[2]));
+                    currentLine = reader.ReadLine();
+                }
+                reader.Close();
+            }
+            return path;
+        }
+    }
+}
